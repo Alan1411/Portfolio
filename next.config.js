d019e77725c421950e4dadd27710c09a54e4efcb
Alan1411 <89741257+Alan1411@users.noreply.github.com>
2026-09-10 20:36:14 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,19 @@ const nextConfig = {
       },
     ],
   },
+
+  async rewrites() {
+    return [
+      {
+        source: '/map',
+        destination: 'http://45.133.9.100:8100',
+      },
+      {
+        source: '/map/:path*',
+        destination: 'http://45.133.9.100:8100/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
